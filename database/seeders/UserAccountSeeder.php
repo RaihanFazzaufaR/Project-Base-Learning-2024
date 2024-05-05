3e77b990bff71a087f066e238c570f46449a0019
PHase00 <113923045+PHase00@users.noreply.github.com>
2024-05-05 20:39:07 +0700

    add little data on seeder

--- database/seeders/UserAccountSeeder.php
+++ database/seeders/UserAccountSeeder.php
@@ -18,7 +18,8 @@ class UserAccountSeeder extends Seeder
             ['user_id'=>1, 'email'=>'fazzaufa94raihan@gmail.com','username'=>'RaihanFR', 'password'=>Hash::make('123456'), 'id_penduduk'=>1],
             ['user_id'=>2, 'email'=>'user1test@gmail.com','username'=>'ZakariaM', 'password'=>Hash::make('123456'), 'id_penduduk'=>3],
             ['user_id'=>3, 'email'=>'user2test@gmail.com','username'=>'LuckyK', 'password'=>Hash::make('123456'), 'id_penduduk'=>6],
-            ['user_id'=>4, 'email'=>'user3test@gmail.com','username'=>'SonyF', 'password'=>Hash::make('123456'), 'id_penduduk'=>8]
+            ['user_id'=>4, 'email'=>'user3test@gmail.com','username'=>'SonyF', 'password'=>Hash::make('123456'), 'id_penduduk'=>8],
+            ['user_id'=>5, 'email'=>'user4test@gmail.com', 'username' => 'TirtaNR', 'password'=>Hash::make('123456'), 'id_penduduk'=>10]
         ];
         DB::table('tb_useraccount')->insert($data);
     }
