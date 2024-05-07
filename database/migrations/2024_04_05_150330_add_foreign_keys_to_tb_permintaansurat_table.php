da9d3f73a63869ebe0413833a96097543f84564e
PHase00 <113923045+PHase00@users.noreply.github.com>
2024-05-07 16:17:53 +0700

    database permintaan surat fixed

--- database/migrations/2024_04_05_150330_add_foreign_keys_to_tb_permintaansurat_table.php
+++ database/migrations/2024_04_05_150330_add_foreign_keys_to_tb_permintaansurat_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tb_permintaansurat', function (Blueprint $table) {
-            $table->foreign(['peminta_id'], 'tb_permintaansurat_ibfk_1')->references(['nik'])->on('tb_penduduk')->onUpdate('restrict')->onDelete('restrict');
+            $table->foreign(['peminta_id'], 'tb_permintaansurat_ibfk_1')->references(['id_penduduk'])->on('tb_penduduk')->onUpdate('restrict')->onDelete('restrict');
             $table->foreign(['template_id'], 'tb_permintaansurat_ibfk_2')->references(['template_id'])->on('tb_template')->onUpdate('restrict')->onDelete('restrict');
         });
     }
