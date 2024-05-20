4b4f63d20b26d07e9623107ca3731111c66b5890
Lucky Kurniawan Langoday <luckykrnwn17@gmail.com>
2024-05-20 15:58:02 +0700

    index bansos

--- resources/views/Bansos/index.blade.php
+++ resources/views/Bansos/index.blade.php
@@ -14,8 +14,8 @@
     {{-- select option --}}
     {{-- opsi yang dipilih berdasarkan kategori bansos yg diterima --}}
     <form class=" flex items-center gap-5  ">
-        <label for="countries" class="block text-sm items-center h-full  font-medium text-gray-900 dark:text-white">Kategori</label>
-        <select id="countries" class="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <label for="kategori" class="text-base text-[#2d5523] font-semibold dark:text-white w-fit justify-center flex items-center">Kategori</label>
+        <select id="kategori" class="h-fit py-3 text-center border-[3px] border-[#2d5523] text-[#2d5523] text-base rounded-lg bg-gray-50 focus:ring-yellow-500 focus:border-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500 ">
           <option selected>Pilih Kategori</option>
           <option value="US">PKH</option>
           <option value="CA">PKH</option>
