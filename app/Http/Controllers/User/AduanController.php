9dd8a0487a28501007c90e1d505df0ca77ebcbf5
PHase00 <113923045+PHase00@users.noreply.github.com>
2024-05-21 16:48:19 +0700

    view and controller user aduan

--- app/Http/Controllers/User/AduanController.php
+++ app/Http/Controllers/User/AduanController.php
@@ -4,12 +4,49 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AduanModel;
 
 class AduanController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $menu = 'Aduan';
-        return view('aduan.index', compact('menu'));
+        $aduans = AduanModel::query()->where('status', 'selesai');
+
+        if ($request->filled('search') && $request->prioritas != "") {
+            $aduans->where(function ($query) use ($request) {
+                $query->where('judul', 'like', '%' . $request->search . '%')
+                    ->where('prioritas', $request->prioritas);
+            });
+        } elseif ($request->filled('search')) {
+            $aduans->where('judul', 'like', '%' . $request->search . '%');
+        } elseif ($request->prioritas != "") {
+            $aduans->where('prioritas', $request->prioritas);
+        }
+
+        $aduans = $aduans->paginate(10)->withQueryString();
+
+        return view('aduan.index', compact('menu', 'aduans'));
+    }
+
+    public function indexAduanku(Request $request)
+    {
+        $menu = 'Aduanku';
+        $aduans = AduanModel::query()->where('pengadu_id', auth()->user()->id);
+
+        if ($request->filled('search') && $request->prioritas != "") {
+            $aduans->where(function ($query) use ($request) {
+                $query->where('judul', 'like', '%' . $request->search . '%')
+                    ->where('prioritas', $request->prioritas);
+            });
+        } elseif ($request->filled('search')) {
+            $aduans->where('judul', 'like', '%' . $request->search . '%');
+        } elseif ($request->prioritas != "") {
+            $aduans->where('prioritas', $request->prioritas);
+        }
+
+        $aduans = $aduans->paginate(10)->withQueryString();
+
+        return view('aduan.index', compact('menu', 'aduans'));
     }
 }
