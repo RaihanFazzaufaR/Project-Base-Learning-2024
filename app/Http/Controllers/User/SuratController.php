ddf26cb791a221b6d963fc278faa8eece00748d7
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-05-22 05:15:35 +0700

    View data Surat-Ku

--- app/Http/Controllers/User/SuratController.php
+++ app/Http/Controllers/User/SuratController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\PermintaanSuratModel;
 
 class SuratController extends Controller
-{
+{     
     public function index()
     {
         $menu = 'Surat';
@@ -14,11 +15,20 @@ class SuratController extends Controller
         // return view('Surat.formSKPindah', compact('menu'));
         return view('Surat.formSKkematian', compact('menu'));
     }
+    
     public function suratku()
     {
         $menu = 'Surat';
-        // return view('Surat.formSK', compact('menu'));
-        return view('Surat.surat-ku', compact('menu'));
-        // return view('Surat.formSKkematian', compact('menu'));
+        $permintaanSurat = PermintaanSuratModel::select('tb_permintaansurat.*', 'tb_penduduk.nama', 'tb_template.jenisSurat')
+        ->join('tb_penduduk', 'tb_permintaansurat.peminta_id', '=', 'tb_penduduk.id_penduduk')
+        ->join('tb_template', 'tb_permintaansurat.template_id', '=', 'tb_template.template_id')
+        // ->where('tb_permintaansurat.status', 'menunggu') // Hanya data dengan status 'menunggu'
+        ->orderBy('tb_permintaansurat.minta_tanggal', 'desc') // Urutkan berdasarkan tanggal terbaru
+        ->paginate(10);
+
+        return view('Surat.surat-ku', compact('permintaanSurat','menu'));
+        // // return view('Surat.formSK', compact('menu'));
+        // return view('Surat.surat-ku', compact('menu'));
+        // // return view('Surat.formSKkematian', compact('menu'));
     }
 }
