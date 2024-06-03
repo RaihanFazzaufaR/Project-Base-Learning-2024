1678a7bdde1b41756342c040bc497fe713cda041
Lucky Kurniawan Langoday <luckykrnwn17@gmail.com>
2024-06-03 17:35:46 +0700

    fix controller

--- app/Http/Controllers/User/PendudukController.php
+++ app/Http/Controllers/User/PendudukController.php
@@ -24,11 +24,11 @@ class PendudukController extends Controller
                 'kk.rt',
                 'kk.alamat'
             )
-            ->join('tb_kartukeluarga as kk', 'tb_penduduk.id_kartuKeluarga', '=', 'kk.id_kartuKeluarga');
+            ->join('tb_kartukeluarga as kk', 'tb_penduduk.id_kartuKeluarga', '=', 'kk.id_kartuKeluarga')
+            ->where('nik', '!=', '0000000000000001');
 
 
         $rts = DB::table('tb_kartukeluarga')->select('rt')->distinct()->pluck('rt');
-
 
         if ($request->has('filterRT')) {
             $query->where('kk.rt', $request->filterRT);
@@ -40,35 +40,7 @@ class PendudukController extends Controller
     }
 
     public function getDataByRT($rt = null)
-{
-    $query = DB::table('tb_penduduk')
-        ->select(
-            DB::raw("CONCAT(SUBSTRING(tb_penduduk.nik, 1, 4), '**********', SUBSTRING(tb_penduduk.nik, -2)) AS nik"),
-            'tb_penduduk.nama',
-            'tb_penduduk.statusPenduduk',
-            'kk.rt',
-            'kk.alamat'
-        )
-        ->join('tb_kartukeluarga as kk', 'tb_penduduk.id_kartuKeluarga', '=', 'kk.id_kartuKeluarga');
-
-    if ($rt) {
-        $query->where('kk.rt', $rt);
-    }
-
-    // Mengambil nilai RT yang unik dari tabel tb_kartukeluarga
-    $rts = DB::table('tb_kartukeluarga')->distinct()->pluck('rt');
-    $menu = 'Penduduk';
-    $penduduks = $query->paginate(10);
-
-    return view('penduduk.index', compact('menu', 'rts', 'penduduks'));
-}
-
-    
-    public function search(Request $request)
     {
-        $searchTerm = $request->input('search');
-        $menu = 'Penduduk';
-        
         $query = DB::table('tb_penduduk')
             ->select(
                 DB::raw("CONCAT(SUBSTRING(tb_penduduk.nik, 1, 4), '**********', SUBSTRING(tb_penduduk.nik, -2)) AS nik"),
@@ -78,13 +50,43 @@ class PendudukController extends Controller
                 'kk.alamat'
             )
             ->join('tb_kartukeluarga as kk', 'tb_penduduk.id_kartuKeluarga', '=', 'kk.id_kartuKeluarga')
-            ->where('tb_penduduk.nama', 'LIKE', "%{$searchTerm}%");
-        
+            ->where('nik', '!=', '0000000000000001');
+
+        if ($rt) {
+            $query->where('kk.rt', $rt);
+        }
+
+        // Mengambil nilai RT yang unik dari tabel tb_kartukeluarga
+        $rts = DB::table('tb_kartukeluarga')->distinct()->pluck('rt');
+        $menu = 'Penduduk';
+        $penduduks = $query->paginate(10);
+
+        return view('penduduk.index', compact('menu', 'rts', 'penduduks'));
+    }
+
+
+    public function search(Request $request)
+    {
+        $searchTerm = $request->input('search');
+        $menu = 'Penduduk';
+
+        $query = DB::table('tb_penduduk')
+            ->select(
+                DB::raw("CONCAT(SUBSTRING(tb_penduduk.nik, 1, 4), '**********', SUBSTRING(tb_penduduk.nik, -2)) AS nik"),
+                'tb_penduduk.nama',
+                'tb_penduduk.statusPenduduk',
+                'kk.rt',
+                'kk.alamat'
+            )
+            ->join('tb_kartukeluarga as kk', 'tb_penduduk.id_kartuKeluarga', '=', 'kk.id_kartuKeluarga')
+            ->where('tb_penduduk.nama', 'LIKE', "%{$searchTerm}%")
+            ->where('nik', '!=', '0000000000000001');
+
         $penduduks = $query->paginate(10);
         $rts = DB::table('tb_kartukeluarga')->distinct()->pluck('rt');
-        
+
         $notification = $penduduks->isEmpty() ? 'Data Tidak Ditemukan' : null;
-        
+
         return view('Penduduk.index', compact('menu', 'penduduks', 'notification', 'rts'));
     }
 }
