5bc6955eb17e374214547948a6236d67acceb3bf
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-06-04 16:49:03 +0700

    fix notifikasi admin

--- app/View/Components/NavbarAdmin.php
+++ app/View/Components/NavbarAdmin.php
@@ -2,6 +2,8 @@
 
 namespace App\View\Components;
 
+use App\Models\JadwalModel;
+use App\Models\UmkmModel;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Contracts\View\View;
@@ -29,11 +31,9 @@ class NavbarAdmin extends Component
     {
         $today = Carbon::today()->toDateString();
         $sevenDaysAgo = Carbon::today()->subDays(7)->toDateString();
-        $id = auth()->user()->id_penduduk ?? 0;
-        // $data = PendudukModel::with(['jadwal', 'umkm'])->whereDate('updated_at', $today)->get();
-        $jadwalQuery = DB::table('tb_jadwal')
-            ->join('tb_penduduk', 'tb_jadwal.pembuat_id', '=', 'tb_penduduk.id_penduduk')
-            ->join('tb_useraccount', 'tb_jadwal.pembuat_id', '=', 'tb_useraccount.id_penduduk')
+
+        $jadwalQuery = JadwalModel::join('tb_penduduk', 'tb_jadwal.pembuat_id', '=', 'tb_penduduk.id_penduduk')
+            ->join('tb_useraccount', 'tb_penduduk.id_penduduk', '=', 'tb_useraccount.id_penduduk')
             ->select(
                 DB::raw('tb_jadwal.alasan_tolak as reason'),
                 'tb_jadwal.updated_at',
@@ -45,12 +45,10 @@ class NavbarAdmin extends Component
             )
             ->whereDate('tb_jadwal.updated_at', '<=', $today)
             ->whereDate('tb_jadwal.updated_at', '>=', $sevenDaysAgo)
-            ->where('tb_jadwal.pembuat_id', $id)
             ->where('tb_jadwal.status', 'diproses');
 
-        $umkmQuery = DB::table('tb_umkm')
-            ->join('tb_penduduk', 'tb_umkm.id_pemilik', '=', 'tb_penduduk.id_penduduk')
-            ->join('tb_useraccount', 'tb_umkm.id_pemilik', '=', 'tb_useraccount.id_penduduk')
+        $umkmQuery = UmkmModel::join('tb_penduduk', 'tb_umkm.id_pemilik', '=', 'tb_penduduk.id_penduduk')
+            ->join('tb_useraccount', 'tb_penduduk.id_penduduk', '=', 'tb_useraccount.id_penduduk')
             ->select(
                 DB::raw('tb_umkm.alasan_rw as reason'),
                 'tb_umkm.updated_at',
@@ -62,11 +60,11 @@ class NavbarAdmin extends Component
             )
             ->whereDate('tb_umkm.updated_at', '<=', $today)
             ->whereDate('tb_umkm.updated_at', '>=', $sevenDaysAgo)
-            ->where('tb_umkm.status', 'diproses')
-            ->where('tb_umkm.id_pemilik', $id);
+            ->where('tb_umkm.status', 'diproses');
 
-        // Combining the queries with unionAll
         $data = $jadwalQuery->unionAll($umkmQuery)->orderBy('updated_at', 'desc')->get();
+
+        // dd($umkmQuery);
 
         // dd($data);
         foreach ($data as  $value) {
