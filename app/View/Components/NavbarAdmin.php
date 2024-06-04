127ca40f67fb9573450263f212ba15824fe54f30
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-06-04 23:09:50 +0700

    fix notification for RT & RW in admin view

--- app/View/Components/NavbarAdmin.php
+++ app/View/Components/NavbarAdmin.php
@@ -32,7 +32,24 @@ class NavbarAdmin extends Component
         $today = Carbon::today()->toDateString();
         $sevenDaysAgo = Carbon::today()->subDays(7)->toDateString();
 
-        $jadwalQuery = JadwalModel::join('tb_penduduk', 'tb_jadwal.pembuat_id', '=', 'tb_penduduk.id_penduduk')
+        $jadwalQueryRT = JadwalModel::join('tb_penduduk', 'tb_jadwal.pembuat_id', '=', 'tb_penduduk.id_penduduk')
+            ->join('tb_useraccount', 'tb_penduduk.id_penduduk', '=', 'tb_useraccount.id_penduduk')
+            ->join('tb_kartukeluarga', 'tb_penduduk.id_kartuKeluarga', '=', 'tb_kartukeluarga.id_kartuKeluarga')
+            ->select(
+                DB::raw('tb_jadwal.alasan_tolak as reason'),
+                'tb_jadwal.updated_at',
+                'tb_jadwal.status',
+                DB::raw('tb_jadwal.pembuat_id as id'),
+                DB::raw("'tb_jadwal' as source"),
+                'tb_penduduk.nama as nama',
+                'tb_useraccount.image as image'
+            )
+            ->whereDate('tb_jadwal.updated_at', '<=', $today)
+            ->whereDate('tb_jadwal.updated_at', '>=', $sevenDaysAgo)
+            ->where('tb_jadwal.status', 'diproses')
+            ->where('tb_kartuKeluarga.rt', auth()->user()->penduduk->kartukeluarga->rt);
+
+        $jadwalQueryRW = JadwalModel::join('tb_penduduk', 'tb_jadwal.pembuat_id', '=', 'tb_penduduk.id_penduduk')
             ->join('tb_useraccount', 'tb_penduduk.id_penduduk', '=', 'tb_useraccount.id_penduduk')
             ->select(
                 DB::raw('tb_jadwal.alasan_tolak as reason'),
@@ -62,7 +79,8 @@ class NavbarAdmin extends Component
             ->whereDate('tb_umkm.updated_at', '>=', $sevenDaysAgo)
             ->where('tb_umkm.status', 'diproses');
 
-        $data = $jadwalQuery->unionAll($umkmQuery)->orderBy('updated_at', 'desc')->get();
+        
+        $data = (auth()->user()->penduduk->jabatan == 'Ketua RW') ? $jadwalQueryRW->unionAll($umkmQuery)->orderBy('updated_at', 'desc')->get() : $jadwalQueryRT->get();
 
         // dd($umkmQuery);
 
