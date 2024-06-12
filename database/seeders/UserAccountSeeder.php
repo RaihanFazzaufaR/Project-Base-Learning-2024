6a3fc42a638c06cff699de72d39f5865797aa8f9
PHase00 <113923045+PHase00@users.noreply.github.com>
2024-06-12 12:36:24 +0700

    little fix

--- database/seeders/UserAccountSeeder.php
+++ database/seeders/UserAccountSeeder.php
@@ -21,7 +21,7 @@ class UserAccountSeeder extends Seeder
             ['user_id'=>4, 'email'=>'2241720168@student.polinema.ac.id','username'=>'LuckyK', 'password'=>Hash::make('12345678'), 'id_penduduk'=>7, 'id_level'=>2, 'image'=>'user3.jpg'],
             ['user_id'=>5, 'email'=>'sonywib04@gmail.com','username'=>'SonyF', 'password'=>Hash::make('12345678'), 'id_penduduk'=>9, 'id_level'=>2, 'image'=>null],
             ['user_id'=>6, 'email'=>'2241720045@student.polinema.ac.id', 'username' => 'TirtaNR', 'password'=>Hash::make('12345678'), 'id_penduduk'=>23, 'id_level'=>2, 'image'=>null],
-            ['user_id'=>7, 'email'=>'user5test@gmail.com', 'username' => 'AbdulN', 'password'=>Hash::make('123456'), 'id_penduduk'=>8,'id_level'=>3, 'image'=>null],
+            ['user_id'=>7, 'email'=>'user5test@gmail.com', 'username' => 'AbdulN', 'password'=>Hash::make('12345678'), 'id_penduduk'=>8,'id_level'=>3, 'image'=>null],
         ];
         DB::table('tb_useraccount')->insert($data);
     }
